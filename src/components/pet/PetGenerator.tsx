3ad5523dcde66af65aa1116ce46db9f5c2427db6
signal7051-art <signal7051@gmail.com>
2026-03-12 23:08:07 +0900

    Update PetGenerator.tsx

--- src/components/pet/PetGenerator.tsx
+++ src/components/pet/PetGenerator.tsx
@@ -205,6 +205,7 @@ export default function PetGenerator() {
 				<Switch
 				  checked={randomTotal}
 				  onCheckedChange={setRandomTotal}
+				  className="border border-gray-400 bg-white data-[state=checked]:bg-green-500 hover:border-gray-600 transition-colors"
 				/>
 				<span>총합 랜덤 (-2~+3)</span>
 			  </div>
@@ -213,6 +214,7 @@ export default function PetGenerator() {
 				<Switch
 				  checked={randomInitial}
 				  onCheckedChange={setRandomInitial}
+				  className="border border-gray-400 bg-white data-[state=checked]:bg-green-500 hover:border-gray-600 transition-colors"
 				/>
 				<span>초기값 랜덤 (-2~+2)</span>
 			  </div>
